5c4dee9936c1ee76aaee0abfb88ffd16c9a38d18
mairelis-montilla <mairelismontilla@gmail.com>
2021-05-16 20:38:05 -0500

    feat header component

---
+++ src/components/header.vue
@@ -0,0 +1,34 @@
+<template>
+<section class="header-container">
+<h2>
+People of Start Wars
+</h2>
+
+</section>
+
+</template>
+
+<script>
+  export default {
+    name:'Header',
+  }
+</script>
+
+<style scoped>
+.header-container{
+  background-color:#121212;
+  height: 90px;
+  position:relative;
+  top:-8px;
+  left:-8px;
+  width: 100vw;
+}
+.header-container >h2{
+  color:white;
+  display:flex;
+  justify-content:center;
+  font-size:17px;
+  margin: 0;
+  padding:30px;
+}
+</style>
